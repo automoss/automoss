1cddaeea50f01d797520fcde79337116ba2d60df
Joshua Lochner <joshua@joshualochner.com>
2021-08-23 14:02:39 +0200

    Add basic parsing of Moss Results

--- automoss/apps/core/moss.py
+++ automoss/apps/core/moss.py
@@ -1,6 +1,11 @@
 import socket
 import os
 import re
+import requests
+
+import numpy as np
+import asyncio
+import aiohttp
 
 
 class MossAPIWrapper:
@@ -68,3 +73,78 @@ class MossAPIWrapper:
 
     def _send_string(self, text):
         return self._send_raw(f'{text}\n'.encode())
+
+
+class MatchItem:
+    # Meaningless alone, must be paired with another match item in Match class
+    def __init__(self, id, percentage, html):
+        self.id = id
+        self.percentage = float(percentage)
+        self.html = html
+
+        self._parse_from_html(html)
+
+    def _parse_from_html(self, html):
+        pass  # TODO
+
+    def __str__(self):
+        return f'({self.id}, {self.percentage}%)'
+
+
+class Match:
+    def __init__(self, first, second, lines_matched):
+        self.first = first
+        self.second = second
+        self.lines_matched = int(lines_matched)
+
+    def __str__(self):
+        return f'{self.first} : {self.second}% | {self.lines_matched}'
+
+
+class MossResult:
+
+    def __init__(self, url):
+        # Parse a moss result from a URL
+        self.url = url
+        self.matches = []
+
+        self._parse_from_url(url)
+
+    # https://stackoverflow.com/a/54878794
+    async def fetch(self, session, url):
+        async with session.get(url) as resp:
+            return await resp.text()
+
+    async def fetch_concurrent(self, urls):
+        loop = asyncio.get_event_loop()
+        async with aiohttp.ClientSession() as session:
+            tasks = [loop.create_task(self.fetch(session, u)) for u in urls]
+            return [await result for result in asyncio.as_completed(tasks)]
+
+    def _generate_urls(self, base_url, num_matches):
+        for i in range(num_matches):
+            # yield f'{base_url}/match{i}.html'
+            for j in (0, 1):
+                yield f'{base_url}/match{i}-{j}.html'
+
+    _MATCH_LINK = r'<A[^>]+>(\S+)\s+\((\d+)%\)</A>[^<]+<TD'
+    _PARSE_MATCH = f'{_MATCH_LINK}>{_MATCH_LINK}[^>]+>(\d+)'
+
+    def _parse_from_url(self, url):
+
+        initial_info = requests.get(url, verify=False)
+        html = initial_info.text
+
+        matches = re.findall(self._PARSE_MATCH, html)
+        urls = self._generate_urls(url, len(matches))
+
+        data = asyncio.run(self.fetch_concurrent(urls))
+        responses = np.reshape(data, (-1, 2))
+
+        for match, response in zip(matches, responses):
+            left = MatchItem(*match[0:2], response[0])
+            right = MatchItem(*match[2:4], response[1])
+            self.matches.append(Match(left, right, match[4]))
+
+    def json(self):
+        return {'url': self.url}
