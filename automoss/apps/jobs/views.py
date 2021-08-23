c019415eb88f7c67843531e15eaa5921aea55a06
Joshua Lochner <joshua@joshualochner.com>
2021-08-23 21:43:18 +0200

    Add logic for uploading files to moss from the new endpoint

--- automoss/apps/jobs/views.py
+++ automoss/apps/jobs/views.py
@@ -1,11 +1,45 @@
+from ..utils.encoding import base64_decode
+from .tasks import upload_to_moss
 from django.shortcuts import render
+from django.http.response import JsonResponse
 
 # Jobs Index View
 def index(request):
-    context = {"jobs": [(1, "Job 1"), (2, "Job 2"), (3, "Job 3"), (4, "Job 4"), (5, "Job 5")]}
-    return render(request, "jobs/index.html", context)
+    context = {"jobs": [(1, "Job 1"), (2, "Job 2"),
+                        (3, "Job 3"), (4, "Job 4"), (5, "Job 5")]}
+    return render(request, "jobs.html", context)
 
-# Jobs Index View
+
 def new(request):
-    context = {}
-    return render(request, "jobs/new.html", context)
+    if request.method == 'POST':
+
+        # TODO get from database
+        moss_user_id = 1
+
+        # TODO get from request
+        language = 'python'
+
+        file_info = {
+            'files': [],
+            'base_files': []
+        }
+
+        for file_type in file_info:
+            for f in request.FILES.getlist(file_type):
+                # TODO add validation (extensions, size, etc.)
+
+                # JSON serialise file information
+                file_info[file_type].append({
+                    'name': base64_decode(f.name),
+                    'file': f.read()
+                })
+
+        upload_to_moss.delay(moss_user_id, language=language, **file_info)
+
+        data = {}  # Return useful information
+        return JsonResponse(data, status=200)
+
+    else:
+
+        context = {}
+        return render(request, "jobs/new.html", context)
