fb4bee9f0bebceeeb25d9616e96195cd5431d42f
Carl Combrinck <carljcombrinck@gmail.com>
2021-08-23 02:09:53 +0200

    Add app name field and route comments

--- automoss/apps/jobs/urls.py
+++ automoss/apps/jobs/urls.py
@@ -2,7 +2,11 @@ from django.urls import path, include
 
 from . import views
 
+app_name = 'jobs'
+
 urlpatterns = [
+    # Jobs Index - View Jobs
     path('', views.index, name='index'),
+    # Reports
     path('<int:id>/report', include('automoss.apps.reports.urls'))
 ]
