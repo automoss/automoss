3b474a74d6d33573058dce0f5ddd89ae3dfcb63d
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-08-23 14:46:45 +0200

    Add new job template

--- automoss/apps/jobs/views.py
+++ automoss/apps/jobs/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     context = {"jobs": [(1, "Job 1"), (2, "Job 2"), (3, "Job 3"), (4, "Job 4"), (5, "Job 5")]}
     return render(request, "jobs/index.html", context)
+
+# Jobs Index View
+def new(request):
+    context = {}
+    return render(request, "jobs/new.html", context)
