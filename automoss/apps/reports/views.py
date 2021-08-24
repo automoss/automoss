57c635a6de74e0c3d5ef1dca3dae2e499b5517c0
Carl Combrinck <carljcombrinck@gmail.com>
2021-08-24 02:16:53 +0200

    Add database logic and report-specific view

--- automoss/apps/reports/views.py
+++ automoss/apps/reports/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import MOSSReport, GeneratedReport
 
-# Create your views here.
-def index(request, id=-1):
-    context = {"id": id}
+def index(request, job_id):
+    """ Renders view of MOSS Job Report"""
+    report = MOSSReport.objects.get(job__job_id=job_id)
+    context = {"report": report}
     return render(request, 'reports/index.html', context)
+
+def report(request, job_id, report_id):
+    """ Renders view of user-generated Report"""
+    context = {"report": GeneratedReport.objects.get(report_id=report_id)}
+    return render(request, 'reports/report.html', context)
