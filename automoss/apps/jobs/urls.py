84f2b2f13e4876fd66fd9d8d287311d9a182e1fa
Carl Combrinck <carljcombrinck@gmail.com>
2021-08-24 02:11:31 +0200

    Update UUID routes

--- automoss/apps/jobs/urls.py
+++ automoss/apps/jobs/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'jobs'
 
 urlpatterns = [
+    # Reports
+    path('<uuid:job_id>/report/', include('automoss.apps.reports.urls')),
     # Jobs Index - View Jobs
     path('', views.index, name='index'),
-    # Reports
-    path('<int:id>/report', include('automoss.apps.reports.urls'))
 ]
