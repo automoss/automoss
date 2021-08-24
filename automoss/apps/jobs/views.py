78e90ace8f04b74591503188fb213cbd6455b679
Carl Combrinck <carljcombrinck@gmail.com>
2021-08-24 02:16:11 +0200

    Add database retrieval of jobs

--- automoss/apps/jobs/views.py
+++ automoss/apps/jobs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Job
 
 # Jobs Index View
 def index(request):
-    context = {"jobs": [(1, "Job 1"), (2, "Job 2"), (3, "Job 3"), (4, "Job 4"), (5, "Job 5")]}
+    context = {"jobs" : Job.objects.all()}
     return render(request, "jobs/index.html", context)
