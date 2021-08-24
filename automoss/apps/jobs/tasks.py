2d56dcee4d5b7958ee490ba95b201f61ee5b30ed
Joshua Lochner <joshua@joshualochner.com>
2021-08-24 14:49:30 +0200

    Improve Moss task processing

--- automoss/apps/jobs/tasks.py
+++ automoss/apps/jobs/tasks.py
@@ -1,10 +1,8 @@
 
+from .models import Job
 from django.core.files.uploadedfile import UploadedFile
 from ..core.moss import (
-    MossResult,
-    MossException,
-    UnsupportedLanguage,
-    MossAPIWrapper
+    MOSS,
 )
 import os
 import time
@@ -14,66 +12,33 @@ logger = get_task_logger(__name__)
 
 
 @task(name='Upload')
-def upload_to_moss(user_id, language='c', files=None, base_files=None,
-                   is_directory=False, experimental=False,
-                   max_matches_until_ignore=1000000, num_to_show=1000000, comment=''):
+def process_job(job_id):
     """Basic interface for generating a report from MOSS"""
 
-    # TODO validate language and/or auto detect language
-    # if language not in self.supported_languages():
-    #     raise UnsupportedLanguage(language)
+    job = Job.objects.get(job_id=job_id)
+    base_dir = os.path.join('media', str(job.job_id), 'uploads')
 
-    url = None
-    try:
-        moss = MossAPIWrapper(user_id)
-        moss.connect()  # TODO retries
+    paths = {
+        'files': [],
+        'base_files': []
+    }
+    for file_type in paths:
+        path = os.path.join(base_dir, file_type)
+        if os.path.isdir(path):
+            paths[file_type] = [os.path.join(path, k)
+                                for k in os.listdir(path)]
 
-        if files is None:  # No files supplied
-            raise MossException
+    moss_id = 1  # TODO get user who owns this job
+    language = 'python'  # TODO use job.language
 
-        if base_files is None:
-            base_files = []
-
-        # Set options
-        moss.set_directory(is_directory)
-        moss.set_experimental(experimental)
-        moss.set_max_matches(max_matches_until_ignore)
-        moss.set_num_to_show(num_to_show)
-        moss.set_language(language)
-
-        data = moss.read()
-
-        # Double check on server-side that language is accepted
-        if data == 'no':
-            raise UnsupportedLanguage(language)  # Unsupported language
-        elif data != 'yes':
-            pass
-
-        # TODO determine progress
-
-        # Upload base files
-        for base_file in base_files:
-            moss.upload_raw_base_file(
-                base_file['name'], base_file['file'].encode(), language)
-
-        # Upload submissions
-        for index, file_info in enumerate(files, start=1):
-            moss.upload_raw_file(
-                file_info['name'], file_info['file'].encode(), language, index)
-
-        # Read and return data
-        url = moss.generate_url(comment)
-
-        # TODO check for errors
-        # b'Error: No files uploaded to compare.\n'
-
-    finally:  # Close session as soon as possible
-        moss.close()
-
-    if not url:
-        raise MossException('Unable to extract URL')
-
-    result = MossResult(url)
+    result = MOSS(moss_id).generate(
+        language=language,
+        **paths,
+        max_matches_until_ignore=job.max_until_ignored,
+        num_to_show=job.max_displayed_matches,
+        comment=job.comment,
+        use_basename=True
+    )
 
     # TODO do something with result, e.g., write to DB
 
