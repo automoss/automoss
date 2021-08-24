90c5cb19cacd7761732aaa0c0db36990fbf0341d
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-08-24 20:33:25 +0200

    Update urls to include root

--- automoss/urls.py
+++ automoss/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # Root
+    path('', include('automoss.apps.jobs.urls')),
     # Jobs
     path('jobs/', include('automoss.apps.jobs.urls')),
     # Admin
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
