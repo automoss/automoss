c2ed05ca8d95116185c4ab847ca966f9618bcc49
Joshua Lochner <joshua@joshualochner.com>
2021-08-24 13:10:21 +0200

    Add 127.0.0.1 as an allowed host

--- automoss/settings.py
+++ automoss/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
