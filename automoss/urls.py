d96c68c0d0788260f5689f871f69d506dedfca6a
Carl Combrinck <carljcombrinck@gmail.com>
2021-08-25 00:52:52 +0200

    Remove redundant route

--- automoss/urls.py
+++ automoss/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # Root
-    path('', include('automoss.apps.jobs.urls')),
     # Jobs
     path('jobs/', include('automoss.apps.jobs.urls')),
     # Admin
