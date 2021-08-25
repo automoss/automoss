14f445cf5b52e3f088be3d96cc8138bcc9118e81
Carl Combrinck <carljcombrinck@gmail.com>
2021-08-25 22:03:36 +0200

    Add login and logout views

--- automoss/apps/users/views.py
+++ automoss/apps/users/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as django_login, logout as django_logout, authenticate
+from django.http import Http404
+from django.conf import settings
 
-# Redirect to jobs/
+def login(request):
+    """ Login View : Logs user in if valid credentials supplied """
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return render(request, "users/login.html")
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                django_login(request, user)
+                return redirect(settings.LOGIN_REDIRECT_URL)
+            else:
+                return render(request, "users/login.html", {"errors": "Account diabled!"})
+        else:
+            return render(request, "users/login.html", {"errors": "The username and password entered were incorrect!"})
+    raise Http404("Login route with method {request.METHOD}, not found!")
+
+
 @login_required
-def home(request):
-    return redirect("jobs:index")
+def logout(request):
+    """ Logout View : Log user out """
+    if request.method == "POST":
+        django_logout(request)
+        return render(request, "users/logged_out.html")
+    return Http404("Logout route with method {request.METHOD}, not found!")
+    
+    
