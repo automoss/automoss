334cc2302d6cebcbdc98786bf8f6b90227647702
Joshua Lochner <joshua@joshualochner.com>
2021-08-27 03:27:05 +0200

    Add test case for uploading to moss and parsing the result

--- automoss/apps/core/tests.py
+++ automoss/apps/core/tests.py
@@ -1,16 +1,42 @@
 from unittest import TestCase
-
-from .moss import MossResult
+import os
+from .moss import (
+    MossResult,
+    MOSS,
+    is_valid_moss_url
+)
+from ...defaults import (
+    MAX_UNTIL_IGNORED,
+    MAX_DISPLAYED_MATCHES
+)
 
 
 class TestMossAPI(TestCase):
-    def test_generate(self):
-        pass
-        # TODO
+    def test_upload_and_parse(self):
 
-    def test_parse(self):
-        # TODO - store files locally
-        test_url = 'http://moss.stanford.edu/results/4/7243339897567/'
-        result = MossResult(test_url)
+        base_dir = os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), 'test')
+        paths = {
+            'files': [],
+            'base_files': []
+        }
+        for file_type in paths:
+            path = os.path.join(base_dir, file_type)
+            if os.path.isdir(path):
+                paths[file_type] = [os.path.join(path, k)
+                                    for k in os.listdir(path)]
 
-        self.assertEqual(len(result.matches), 3)
+        if not paths['files']:
+            return
+
+        moss_user_id = 1  # TODO use environment variables
+        result = MOSS(moss_user_id).generate(
+            language='python',
+            **paths,
+            max_matches_until_ignore=MAX_UNTIL_IGNORED,
+            num_to_show=MAX_DISPLAYED_MATCHES,
+            comment='',
+            use_basename=True
+        )
+
+        self.assertTrue(is_valid_moss_url(result.url))
