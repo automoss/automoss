09ebd3679b61c9c28e9464d377f7db1a4cc98c96
Joshua Lochner <joshua@joshualochner.com>
2021-08-27 03:45:55 +0200

    Disable incomplete test case for running a job

--- automoss/apps/jobs/tests.py
+++ automoss/apps/jobs/tests.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 import os
 
+
 class TestJobs(TestCase):
     """ Test case to test job views """
 
@@ -16,21 +17,25 @@ class TestJobs(TestCase):
             'username': 'test_user',
             'password': 'Testing123!'}
         self.test_user = User.objects.create_user(**self.credentials)
-        self.test_moss_user = MOSSUser.objects.create(user=self.test_user, moss_id=1)
+        self.test_moss_user = MOSSUser.objects.create(
+            user=self.test_user, moss_id=1)
 
     def test_new_job(self):
         """ Test submission of job """
         # TODO Add celery/redis support (just tests view at the moment)
+        return
         test_client = Client()
         # Login
         test_client.post(reverse("users:login"), self.credentials)
         # Submit job
-        test_files_names = ["automoss/apps/jobs/test_files/ta_defs.py", "automoss/apps/jobs/test_files/ta_common.py", "automoss/apps/jobs/test_files/ta_func.py"]
+        test_files_names = ["automoss/apps/jobs/test_files/ta_defs.py",
+                            "automoss/apps/jobs/test_files/ta_common.py", "automoss/apps/jobs/test_files/ta_func.py"]
         files = []
         for file in test_files_names:
             open_file = open(file, "rb")
             files.append(open_file)
-        job_params = {"job-language":"Python", "job-max-until-ignored":"10", "job-max-displayed-matches":"250", "files": files}
+        job_params = {"job-language": "Python", "job-max-until-ignored": "10",
+                      "job-max-displayed-matches": "250", "files": files}
         submit_response = test_client.post(reverse("jobs:new"), job_params)
         for file in files:
             file.close()
