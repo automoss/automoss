080baa604090618912f4cffa3bdac8a32732b61b
Joshua Lochner <joshua@joshualochner.com>
2021-08-31 03:29:57 +0200

    Move defaults to settings.py

--- automoss/settings.py
+++ automoss/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from .apps.utils.core import capture_in
+from .apps.utils.core import first
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -65,7 +67,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # Global templates directory
-        'DIRS': [ BASE_DIR / 'automoss' / 'templates'],
+        'DIRS': [BASE_DIR / 'automoss' / 'templates'],
         # Search local application templates directories
         'APP_DIRS': True,
         'OPTIONS': {
@@ -135,19 +137,103 @@ USE_TZ = False
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-#STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CELERY STUFF
+# +-----------------+
+# | CUSTOM SETTINGS |
+# +-----------------+
+
+# Celery variables
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+
+LANGUAGE_CONTEXT = {}
+with capture_in(LANGUAGE_CONTEXT):
+    # Supported languages
+    SUPPORTED_LANGUAGES = {
+        # CODE : (Name, moss_name, [extensions])
+
+        'PY': ('Python', 'python', ['py']),  # pyi, pyc, pyd, pyo, pyw, pyz
+        'JA': ('Java', 'java', ['java']),  # class, jar
+        'CP': ('C++', 'cc', ['C', 'cc', 'cpp', 'cxx', 'c++', 'h', 'H', 'hh', 'hpp', 'hxx', 'h++']),
+        'CX': ('C', 'c', ['c', 'h']),
+        'CS': ('C#', 'csharp', ['cs', 'csx']),
+        'JS': ('Javascript', 'javascript', ['js']),  # cjs, mjs
+        'PL': ('Perl', 'perl', ['pl', 'plx', 'pm', 'xs', 't', 'pod']),
+        'MP': ('MIPS assembly', 'mips', ['asm', 's']),
+
+        # TODO decide which to add, and add extensions
+        # 'LP' : ('Lisp', 'lisp', []),
+        # 'HS' : ('Haskell', 'haskell', []),
+        # 'VB' : ('Visual Basic', 'vb', []),
+        # 'MB' : ('Matlab', 'matlab', []),
+        # 'AA' : ('a8086 assembly', 'a8086', []),
+        # 'VL' : ('Verilog', 'verilog', []),
+        # 'PS' : ('Pascal', 'pascal', []),
+        # 'ML' : ('ML', 'ml', []),
+        # 'AD' : ('Ada', 'ada', []),
+        # 'VH' : ('VHDL', 'vhdl', []),
+        # 'SC' : ('Scheme', 'scheme', []),
+        # 'FT' : ('FORTRAN', 'fortran', ['f', 'for', 'f90']),
+        # 'SP' : ('Spice', 'spice', []),
+        # 'PG' : ('Prolog', 'prolog', ['pl', 'pro', 'P']),
+        # 'PS' : ('PL/SQL', 'plsql', []),
+        # 'AS' : ('ASCII', 'ascii', []) # All?
+    }
+
+MOSS_CONTEXT = {}
+with capture_in(MOSS_CONTEXT):
+    # Moss defaults
+    READABLE_LANGUAGE_MAPPING = {
+        SUPPORTED_LANGUAGES[l][0]: l for l in SUPPORTED_LANGUAGES}
+    DEFAULT_MOSS_SETTINGS = {
+        'max_until_ignored': 10,
+        'max_displayed_matches': 250
+    }
+
+STATUS_CONTEXT = {}
+with capture_in(STATUS_CONTEXT):
+    # Statuses
+    UPLOADING_STATUS = 'UPL'
+    PROCESSING_STATUS = 'PRO'
+    COMPLETED_STATUS = 'COM'
+    FAILED_STATUS = 'FAI'
+
+    STATUSES = {
+        # 'Code': 'Name',
+        UPLOADING_STATUS: 'Uploading',
+        PROCESSING_STATUS: 'Processing',
+        COMPLETED_STATUS: 'Complete',
+        FAILED_STATUS: 'Failed'
+    }
+
+SUBMISSION_CONTEXT = {}
+with capture_in(SUBMISSION_CONTEXT):
+    # Submission types
+    BASE_FILES_NAME = 'base_files'
+    FILES_NAME = 'files'
+
+    SUBMISSION_TYPES = {
+        BASE_FILES_NAME: 'Base file',
+        FILES_NAME: 'Submission'
+    }
+
+# UI Defaults
+UI_CONTEXT = {}
+with capture_in(UI_CONTEXT):
+    POLLING_TIME = 1000  # in milliseconds
+
+# Misc Constants
+UUID_LENGTH = 32
+MAX_COMMENT_LENGTH = 64
