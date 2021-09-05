87922f79953fb08ca71fc0ed4426ae7da23ad5e1
Carl Combrinck <carljcombrinck@gmail.com>
2021-09-06 01:31:34 +0200

    Move JavaScript to external file

---
+++ static/js/registration.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict'
+    // Get all forms needing validation
+    let forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+  
