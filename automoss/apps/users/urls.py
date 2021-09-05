c05bccb98aa020cb8264dbe38ec0025134d1ad05
Carl Combrinck <carljcombrinck@gmail.com>
2021-09-06 00:30:37 +0200

    Add register route

--- automoss/apps/users/urls.py
+++ automoss/apps/users/urls.py
@@ -22,4 +22,5 @@ app_name = "users"
 urlpatterns = [
     path('login/', views.Login.as_view(), name="login"),
     path('logout/', views.Logout.as_view(), name="logout"),
+    path('register/', views.Register.as_view(), name="register"),
 ]
