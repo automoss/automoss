b482fb71b4dc3ba69c83da3b3b2a460e5607039c
Carl Combrinck <carljcombrinck@gmail.com>
2021-09-06 00:30:13 +0200

    Add Registration-related Django forms

---
+++ automoss/apps/users/forms.py
@@ -0,0 +1,75 @@
+import unicodedata
+from .models import User
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth import password_validation
+
+class UsernameField(forms.CharField):
+    """ User name field - based on Django's UsernameField """
+    def to_python(self, value):
+        return unicodedata.normalize('NFKC', super().to_python(value))
+
+    def widget_attrs(self, widget):
+        return {
+            **super().widget_attrs(widget),
+            'autocapitalize': 'none',
+            'autocomplete': 'username',
+        }
+
+class UserCreationForm(forms.ModelForm):
+    """ User registration form - based on Django's UserCreationForm """
+    error_messages = {
+        'password_mismatch': 'The two password fields didn’t match.',
+    }
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text="Enter a password.",
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+        help_text="Re-enter your password.",
+    )
+    class Meta:
+        model = User
+        fields = (
+            'course_code', 
+            'primary_email_address', 
+            'moss_id',
+        )
+        field_classes = {'course_code': UsernameField}
+
+    def _post_clean(self):
+        super()._post_clean()
+        # Validate the password after self.instance is updated with form data
+        # by super().
+        password = self.cleaned_data.get('password2')
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except ValidationError as error:
+                self.add_error('password2', error)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class LoginForm(AuthenticationForm):
+    """ User login form """
