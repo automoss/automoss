26458e52bee723ff4f1843631cfc92c122771ae9
Joshua Lochner <joshua@joshualochner.com>
2021-09-15 03:45:47 +0200

    Improve handling of failed jobs (and retrying)

--- automoss/apps/jobs/tasks.py
+++ automoss/apps/jobs/tasks.py
@@ -1,12 +1,15 @@
 
+import random
 from ..results.models import MOSSResult, Match
 from .models import Job, Submission
 from django.utils.timezone import now
 from django.core.files.uploadedfile import UploadedFile
+from ..moss.pinger import Pinger, LoadStatus
 from ..moss.moss import (
     MOSS,
     Result,
     RecoverableMossException,
+    EmptyResponse,
     FatalMossException,
     is_valid_moss_url
 )
@@ -18,10 +21,15 @@ from ...settings import (
     SUBMISSION_TYPES,
     FILES_NAME,
     JOB_UPLOAD_TEMPLATE,
+    DEBUG,
+
+    MIN_RETRY_TIME,
+    MAX_RETRY_TIME,
+    MAX_RETRY_DURATION,
     EXPONENTIAL_BACKOFF_BASE,
-    MAX_RETRIES,
-    DEBUG
+    FIRST_RETRY_INSTANT
 )
+from ..utils.core import retry
 import os
 import json
 import time
@@ -42,6 +50,15 @@ def process_job(job_id):
     """Basic interface for generating a report from MOSS"""
 
     job = Job.objects.get(job_id=job_id)
+
+    if job.status == COMPLETED_STATUS:
+        # Do not process completed jobs
+        # Necessary because redis stores a list of these jobs in the database when the server goes offline
+        # Alternative: Delete dump.rdb on server start (bad idea)?
+        return
+
+    logger.info(f'Starting job {job_id} with status {job.status}')
+
     job.status = PROCESSING_STATUS
     job.save()
 
@@ -67,10 +84,15 @@ def process_job(job_id):
         job.save()
         return None
 
+    num_attempts = 0
     url = None
     result = None
-    for attempt in range(MAX_RETRIES + 1):
+
+    for attempt, time_to_sleep in retry(MIN_RETRY_TIME, MAX_RETRY_TIME, EXPONENTIAL_BACKOFF_BASE, MAX_RETRY_DURATION, FIRST_RETRY_INSTANT):
+        num_attempts = attempt
+
         try:
+            error = None
             if not is_valid_moss_url(url):
                 # Keep retrying until valid url has been generated
                 # Do not restart whole job if this succeeds but parsing fails
@@ -80,45 +102,57 @@ def process_job(job_id):
                     **paths,
                     max_until_ignored=job.max_until_ignored,
                     max_displayed_matches=job.max_displayed_matches,
-                    use_basename=True
+                    use_basename=True,
                 )
-                print(f'Generated url: "{url}"')
+                logger.info(f'Generated url: "{url}"')
 
             # TODO set status to parsing?
 
-            print('Start parsing report')
+            logger.info('Start parsing report')
             # Parsing and extraction
             result = MOSS.generate_report(url)
-            print('Result finished parsing:', len(
-                result.matches), 'matches detected.')
+            logger.info(
+                f'Result finished parsing: {len(result.matches)} matches detected.')
 
             break  # Success, do not retry
-        
+
         except RecoverableMossException as e:
-            # A recoverable error occurred... retry
-            time_to_sleep = EXPONENTIAL_BACKOFF_BASE ** attempt
-            print(f'(Attempt {attempt}) Error: {e}. Retrying in {time_to_sleep} seconds')
-            time.sleep(time_to_sleep)
-            # TODO ensure sleeping is correct
+            error = e  # Handled below
+
+        except EmptyResponse as e:
+            # Job ended after
+            error = e
+
+            load_status = Pinger.determine_load()
+            if load_status == LoadStatus.NORMAL:
+                logger.debug(
+                    f'Moss is not under load - job ({job_id}) will never finish')
+                break
+
+            elif load_status == LoadStatus.UNDER_LOAD:
+                logger.debug(f'Moss is under load, retrying job ({job_id})')
+            else:
+                logger.debug(f'Moss is down, retrying job ({job_id})')
 
         except FatalMossException as e:
-            break # Will be handled below (result is None)
+            break  # Will be handled below (result is None)
 
-        except Exception:
+        except Exception as e:
             # TODO something catastrophic happened
             # Do some logging here
-            break
+            logger.error(f'Unknown error: {e}')
+            break  # Will be handled below (result is None)
+
+        # We can retry
+        logger.warning(
+            f'(Attempt {attempt}) Error: {error} | Retrying in {time_to_sleep} seconds')
+        time.sleep(time_to_sleep)
 
     # Represents when no more processing of the job will occur
     job.completion_date = now()
 
-    if result is None:  # No result
-        job.status = FAILED_STATUS  # TODO detect failure
-        job.save()
-        return None
-
-    # Success
-    job.status = COMPLETED_STATUS
+    failed = result is None
+    job.status = FAILED_STATUS if failed else COMPLETED_STATUS  # TODO detect failure
     job.save()
 
     if DEBUG:
@@ -132,14 +166,21 @@ def process_job(job_id):
         log_info.update({
             'num_files': num_files,
             'avg_file_size': avg_file_size,
-            'moss_id': job.user.moss_id
+            'moss_id': job.user.moss_id,
+            'num_attempts': num_attempts
         })
+
+        log_info.update(Pinger.ping() or {})
+        logger.debug(f'Job info: {log_info}')
 
         with open(LOG_FILE, 'a+') as fp:
             log_info['duration'] = (
                 log_info['completion_date'] - log_info['start_date']).total_seconds()
             json.dump(log_info, fp, sort_keys=True, default=str)
             print(file=fp)
+
+    if failed:
+        return None
 
     moss_result = MOSSResult.objects.create(
         job=job,
