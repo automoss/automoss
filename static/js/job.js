16ca0a419cd28f9f05bc3ef086b1da34b8fe1abd
Joshua Lochner <joshua@joshualochner.com>
2021-09-16 16:20:37 +0200

    Separate Job class logic into separate file

---
+++ static/js/job.js
@@ -0,0 +1,43 @@
+class Job extends HTMLTableRowElement{
+	constructor(obj) {
+		super()
+		Object.assign(this, obj)
+
+		this.setAttribute('job_id', obj.job_id)
+		this.setAttribute('status', obj.status)
+
+		this.tableComment = document.createElement('td')
+		this.tableComment.innerHTML = this.comment;
+		
+		this.tableLanguage = document.createElement('td')
+		this.tableLanguage.innerHTML = languages[this.language][0];
+
+		this.tableStartDate = document.createElement('td')
+		this.tableStartDate.innerHTML = new Date(this.creation_date).toLocaleString();
+		
+		let tableStatusCell = document.createElement('td')
+		this.tableStatus = document.createElement('span')
+		tableStatusCell.append(this.tableStatus)
+		
+		this.append(this.tableComment)
+		this.append(this.tableLanguage)
+		this.append(this.tableStartDate)
+		this.append(tableStatusCell)
+
+		this.setStatus(this.status)
+	}
+
+	setStatus(newStatus){
+		this.tableStatus.innerHTML = statuses[newStatus]
+		if(newStatus == completedStatus){
+			this.tableComment.innerHTML = `<a href="/jobs/${this.job_id}/result/" style="text-decoration: none;">${this.comment}</a>`
+		}
+
+		this.tableStatus.className = '';
+		
+		let classes = ['badge', statusMapping[newStatus]];
+
+		this.tableStatus.classList.add(...classes)
+	}
+}
+customElements.define('job-row', Job, { extends: 'tr' });
