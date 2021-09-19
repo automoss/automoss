cf24ea622c7335d63a983900c34d2d3bd73fcaf1
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-09-19 18:20:59 +0200

    Refactor job submission

--- static/js/jobSubmission.js
+++ static/js/jobSubmission.js
@@ -1,5 +1,5 @@
 /**
- * Determines whether or not a file has an extension from a list of given extensions.
+ * Determines whether or not a file name ends with an extension from a list of given extensions.
  */
 function hasExtension(fileName, extensions){
 	for (var extension of extensions){
@@ -10,28 +10,24 @@ function hasExtension(fileName, extensions){
 	return false;
 }
 
-const ARCHIVE_EXTENSIONS = ["tar", "tar.gz", "tar.xz", "tar.bz2", "zip", "rar"]
+/**
+ * Determines whether or not a file is an archive.
+ */
 function isArchive(fileName){
-	return hasExtension(fileName, ARCHIVE_EXTENSIONS);
+	return hasExtension(fileName, SUPPORTED_ARCHIVES);
 }
 
-const PROGRAM_EXTENSIONS = {
-	"PY": ["py"], 
-	"JA": ["java"], 
-	"CP": ["C", "cc", "cpp", "cxx", "c++", "h", "H", "hh", "hpp", "hxx", "h++"], 
-	"CX": ["c", "h"], 
-	"CS": ["cs", "csx"], 
-	"JS": ["js"], 
-	"PL": ["pl", "plx", "pm", "xs", "t", "pod"], 
-	"MP": ["asm", "s"]
-};
+/**
+ * Determines whether or not a file is a source file. If a programming language code is specified,
+ * this will return if it is a source file for that particular language.
+ */
 function isSource(fileName, language){
 	let extensions = [];
 	if (language != null){
-		extensions = PROGRAM_EXTENSIONS[language];	
+		extensions = SUPPORTED_LANGUAGES[language];
 	}else{
-		for (let key of Object.keys(PROGRAM_EXTENSIONS)){
-			extensions = extensions.concat(PROGRAM_EXTENSIONS[key]);
+		for (let key of Object.keys(SUPPORTED_LANGUAGES)){
+			extensions = extensions.concat(SUPPORTED_LANGUAGES[key][3]);
 		}
 	}
 	return hasExtension(fileName, extensions);
@@ -46,6 +42,16 @@ function getFileNameFromPath(filePath){
         return filePath.substring(dirSepIndex+1);
     }
     return filePath;
+}
+
+/**
+ * Renames a given file and returns it.
+ */
+ function renameFile(file, newName){
+	return new File([file], newName, {
+		type: file.type,
+		lastModified: file.lastModified,
+	});
 }
 
 /**
@@ -108,6 +114,17 @@ function getRootIndex(files){
 }
 
 /**
+ * Reads the data from a file and returns it.
+ */
+async function readFileData(file){
+	return new Promise((resolve, reject) => {
+		file.readData(function(data){
+			resolve(data);
+		});
+	});
+}
+
+/**
  * Extracts all the source files from a single student's archive, and "stitches" them together.
  */
 async function extractSingle(files, language){
@@ -119,6 +136,32 @@ async function extractSingle(files, language){
 		}
 	}
 	return buffer;
+}
+
+/**
+ * Extract and return a list of files.
+ */
+async function extractFiles(archive) {
+
+	// Wait until formats have been loaded
+	await loadFormatsPromise;
+
+	// Wait until archived file has been opened
+	let openArchivePromise = new Promise((resolve, reject) => {
+		archiveOpenFile(archive, '', (opened) => {
+			resolve(opened);
+		});
+	});
+	let opened = await openArchivePromise;
+
+	// Wait until all the files have been added to a list
+	let files = [];
+	opened.entries.forEach(function(file) {
+		if(file.is_file){
+			files.push(file);
+		}
+	});
+	return files;
 }
 
 /**
@@ -178,9 +221,184 @@ async function extractBatch(files, language, onExtract){
 	}
 }
 
-// Extend scope of functions to rest of WebApp
+// Extend scope of submission functions to rest of WebApp
 window.isArchive = isArchive;
 window.isSource = isSource;
 window.isSingleSubmission = isSingleSubmission;
 window.extractSingle = extractSingle;
+window.extractFiles = extractFiles;
 window.extractBatch = extractBatch;
+
+
+
+
+// Load all supported archive format types
+let loadFormatsPromise = new Promise((resolve, reject) => {
+	loadArchiveFormats(["zip", "tar", "rar"], () => {
+		resolve();
+	});
+});
+
+
+// Document references to job submission elements
+let createJobModalElement = document.getElementById('create-job-modal');
+let createJobModal = new bootstrap.Modal(createJobModalElement);
+let createJobForm = document.getElementById('create-job-form');
+let jobDropZone = document.getElementById('job-drop-zone');
+let jobName = document.getElementById('job-name');
+let jobLanguage = document.getElementById('job-language');
+let jobMaxMatchesUntilIgnored = document.getElementById('job-max-until-ignored');
+let jobMaxMatchesDisplayed = document.getElementById('job-max-displayed-matches');
+let jobErrorMessage = document.getElementById("job-error-message");
+let createJobButton = document.getElementById('create-job-button');
+
+let isDisplayingError = false;
+function displayError(message){
+	if (isDisplayingError){
+		return;
+	}
+	jobErrorMessage.textContent = message;
+	createJobModalElement.classList.add("animated", "shake");
+	isDisplayingError = true;
+
+	setTimeout(function(){
+		jobErrorMessage.textContent = "";
+		createJobModalElement.classList.remove("animated", "shake");
+		isDisplayingError = false;
+	}, 3000);
+}
+
+function overwriteJobData(jobDropZoneFile, name, data){
+	jobDropZoneFile.data.push({name: name, data: data});
+}
+
+createJobForm.onsubmit = async (e) => {
+
+	e.preventDefault();
+
+	// Create a new form (and capture name, language, max matches until ignored and max matches displayed)
+	let jobFormData = new FormData(createJobForm);
+	let counter = 0;
+	for (let jobDropZoneFile of jobDropZone.files){
+		for (let data of jobDropZoneFile.data){
+			jobFormData.append(FILES_NAME, new Blob([data.data]), data.name);
+			counter++;
+		}
+	}
+
+	// Can't submit only 1 student
+	if (counter <= 1){
+		displayError("Please submit more than 1 students' files.");
+		return;
+	}
+
+	// Submit a new job with the created form
+	let result = await fetch(NEW_JOB_URL, {
+		method: 'POST',
+		body: jobFormData,
+	});
+
+	// Receive the result as a json object and add it to the jobs table
+	let json = await result.json();
+	addJob(json);
+	unfinishedJobs.push(json['job_id']);
+
+	// Hide and reset the form data and dropzone
+	createJobModal.hide();
+	createJobForm.reset();
+	jobDropZone.reset();
+};
+
+jobDropZone.onFileAdded = async (jobDropZoneFile) => {
+
+	createJobButton.disabled = true;
+
+	function getExtension(name){
+		return name.split('.').pop();
+	}
+
+	function getProgrammingLanguageId(files){
+		let d = {};
+		for (let key in SUPPORTED_LANGUAGES){
+			d[key] = 0;
+		}
+		for (let file of files){
+			let extension = getExtension(file.name);
+			for (let key in SUPPORTED_LANGUAGES){
+				if(SUPPORTED_LANGUAGES[key][2].includes(extension)){
+					d[key] += 1;
+				}
+			}
+		}
+		return Object.entries(d).reduce((a, b) => a[1] > b[1] ? a : b)[0]
+	}
+
+	let archive = jobDropZoneFile.file;
+	let files = await extractFiles(archive);
+	console.log("0");
+
+	let length = files.length * 2;
+	let counter = 0;
+
+	// Type
+	let single = await isSingleSubmission(files);
+
+	let t0 = performance.now();
+	let langTestFiles = [...files];
+	if (!single){
+		for (let file of files){
+			if (isArchive(file.name)){
+				let tmpArchive = await extractFileNames(file);
+				for (let tmpFile of tmpArchive){
+					langTestFiles.push(tmpFile);
+				}
+				counter++;
+				jobDropZoneFile.setProgress(counter / length);
+			}
+		}
+	}
+
+	let t1 = performance.now();
+	console.log('Took', t1-t0)
+
+	// Programming Language
+	let languageId = getProgrammingLanguageId(langTestFiles);
+	let language = SUPPORTED_LANGUAGES[languageId][0];
+
+	console.log(language)
+	// 
+	if (single){
+		overwriteJobData(jobDropZoneFile, archive.name, await extractSingle(files, languageId));
+		jobDropZoneFile.setProgress(1);
+
+	}else{
+		await extractBatch(files, languageId, (name, data) => {
+			overwriteJobData(jobDropZoneFile, name, data);
+			counter++;
+			jobDropZoneFile.setProgress(counter / length);
+		});
+	}
+
+	// Tags
+	jobDropZoneFile.addTag(single ? "Single" : "Batch", "var(--bs-dark)");
+	jobDropZoneFile.addTag(language, "var(--bs-dark)");
+
+	jobName.value = jobName.value || archive.name.slice(0, archive.name.length - 4);
+	jobLanguage.value = language;
+	createJobButton.disabled = false;
+};
+
+jobDropZone.onFileRemoved = () => {
+	if (jobDropZone.files.length == 0) {
+		jobName.value = "";
+		jobLanguage.selectedIndex = 0;
+		jobMaxMatchesUntilIgnored.value = DEFAULT_MOSS_SETTINGS.max_until_ignored;
+		jobMaxMatchesDisplayed.value = DEFAULT_MOSS_SETTINGS.max_displayed_matches;
+
+		createJobButton.disabled = true;
+	}
+};
+
+jobDropZone.onFileRejected = (reason) => {
+	displayError(reason);
+}
