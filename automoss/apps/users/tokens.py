d72f6dff27ef4453ee2852fc5789f9eb50cb0063
Carl Combrinck <carljcombrinck@gmail.com>
2021-09-22 14:04:39 +0200

    Add token generators for password reset, email and account verification

---
+++ automoss/apps/users/tokens.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+class ConfirmEmailTokenGenerator(PasswordResetTokenGenerator):
+    """ Token generator for generating single-use, expiring tokens for email confirmation """
+    def _make_hash_value(self, email, timestamp):
+        """ Creates string to hash based on user state and info that changes """
+        return f"{email.pk}{email.email}{timestamp}{email.is_verified}"
+
+class UserTokenGenerator(PasswordResetTokenGenerator):
+    """ Token generator for generating single-use, expiring tokens for password changing """
+    def _make_hash_value(self, user, timestamp):
+        """ Creates string to hash based on user state and info that changes """
+        return f"{user.user_id}{user.course_code}{user.moss_id}{timestamp}{user.password}{user.is_verified}"
+
+email_confirmation_token = ConfirmEmailTokenGenerator()
+password_reset_token = UserTokenGenerator()
+confirm_registration_token = UserTokenGenerator()
