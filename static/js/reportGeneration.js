3b6383eabbd49104f41daa3498d2c8eecb6ffdca
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-09-22 15:30:33 +0200

    Add report generation

--- static/js/reportGeneration.js
+++ static/js/reportGeneration.js
@@ -1,19 +1,36 @@
-function downloadHTML(fileName, html) {
+function generateHTML(fileName, html) {
 	var element = document.createElement('a');
 	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(html));
 	element.setAttribute('download', fileName);
 	element.click();
 }
 
-function addDependencies(html, styles, scripts){
-	return styles.join("") + html + scripts.join("");
+async function getElementsByTagNameText(tagName, ref){
+	let elements = document.getElementsByTagName(tagName);
+	let elements_text = "";
+	for (let element of elements){
+		if (element.innerHTML == "" && false){ // TODO: Download external text and add that instead of the link!
+			elements_text += await getExternalText(element[ref]);
+		}else{
+			elements_text += element.outerHTML.replace("/static/", "http://localhost:8000/static/"); // TODO: Change to templated.
+		}
+		elements_text += "\n";
+	}
+	return elements_text;
 }
 
-function downloadReport(){
-	
-	let styles = [];
-	let scripts = [];
-	
+async function getExternalText(url){
+	let response = await fetch(url, {
+		method: "GET"
+	});
+	return await response.text();
+}
 
-	downloadHTML("report.html", addDependencies(html, styles, scripts));
+async function downloadReport(){
+	let links = await getElementsByTagNameText("link", "href");
+	let styles = await getElementsByTagNameText("style");
+	let scripts = await getElementsByTagNameText("script", "src");
+	let report = document.getElementById("report").innerHTML;
+	let html = links + styles + report + scripts
+	generateHTML("report.html", html);
 }
