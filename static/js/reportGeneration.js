9b8e2ce4dd4338723174580086b293355f8d718c
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-09-23 00:27:15 +0200

    Add embedding of external scripts into offline reports

--- static/js/reportGeneration.js
+++ static/js/reportGeneration.js
@@ -1,18 +1,21 @@
-function generateHTML(fileName, html) {
-	var element = document.createElement('a');
+function generate(fileName, html) {
+	let element = document.createElement('a');
 	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(html));
 	element.setAttribute('download', fileName);
 	element.click();
 }
 
-async function getElementsByTagNameText(tagName, ref){
+async function extract(tagName, onExternal){
 	let elements = document.getElementsByTagName(tagName);
 	let elements_text = "";
 	for (let element of elements){
-		if (element.innerHTML == "" && false){ // TODO: Can't use GET request from Google?
-			elements_text += await getExternalText(element[ref]);
+		if (element.hasAttribute("ignoreOnExtract")){
+			continue;
+		}
+		if (element.innerHTML.trim() === ""){
+			elements_text += await onExternal(element);
 		}else{
-			elements_text += element.outerHTML.replace("/static/", "http://localhost:8000/static/"); // TODO: Can remove "replace(...)" when above is solved.
+			elements_text += element.outerHTML;
 		}
 		elements_text += "\n";
 	}
@@ -21,16 +24,27 @@ async function getElementsByTagNameText(tagName, ref){
 
 async function getExternalText(url){
 	let response = await fetch(url, {
-		method: "GET"
+		mode: "no-cors"
 	});
 	return await response.text();
 }
+async function onExternalLink(element){
+	let innerHTML = element.innerHTML;
+	if (element.rel === "stylesheet"){
+		innerHTML = await getExternalText(element["href"]);
+	}
+	return `<style>\n${innerHTML}\n</style>`;
+}
+async function onExternalScript(element){
+	return `<script>\n${await getExternalText(element["src"])}\n</script>`;
+}
 
 async function downloadReport(){
-	let links = await getElementsByTagNameText("link", "href");
-	let styles = await getElementsByTagNameText("style");
-	let scripts = await getElementsByTagNameText("script", "src");
+
+	let extractions = [extract("link", onExternalLink), extract("style"), extract("script", onExternalScript)];	
+	let [links, styles, scripts] = await Promise.all(extractions);
+
 	let report = document.getElementById("report").innerHTML;
 	let html = links + styles + report + scripts
-	generateHTML("report.html", html);
+	generate("report.html", html);
 }
