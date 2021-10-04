4383b4ce90fe53d9ef2e4c42113bced354722602
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-10-04 15:14:20 +0200

    Set DEBUG=False

--- automoss/settings.py
+++ automoss/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
