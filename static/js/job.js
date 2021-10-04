cd24cc9f2890fe0426058019e1aae1cbb68f87c8
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-10-04 15:58:11 +0200

    Comment out jobs info

--- static/js/job.js
+++ static/js/job.js
@@ -1,43 +1,54 @@
 class Job extends HTMLTableRowElement{
     constructor(obj) {
-        super()
-        Object.assign(this, obj)
+        super();
+        Object.assign(this, obj);
 
-        this.setAttribute('job_id', obj.job_id)
-        this.setAttribute('status', obj.status)
-        this.setAttribute('data-bs-toggle', 'collapse');
-        this.setAttribute('data-bs-target', `#job-accordion-${obj.job_id}`);
-        this.style="cursor: pointer;";
+        this.setAttribute('job_id', obj.job_id);
+        this.setAttribute('status', obj.status);
+        // this.setAttribute('data-bs-toggle', 'collapse');
+        // this.setAttribute('data-bs-target', `#job-info-${obj.job_id}`);
+        // this.style="cursor: pointer;";
 
-        // Comment
-        this.tableComment = document.createElement('td')
+        // Name
+        this.tableComment = document.createElement('td');
         this.tableComment.innerHTML = this.comment;
         
         // Language
-        this.tableLanguage = document.createElement('td')
+        this.tableLanguage = document.createElement('td');
         this.tableLanguage.innerHTML = SUPPORTED_LANGUAGES[this.language][0];
 
-        // Start Date
-        this.tableStartDate = document.createElement('td')
+        // Students
+        this.tableStudents = document.createElement('td');
+        this.tableStudents.innerHTML = 1;
+
+
+        // Date Created
+        this.tableStartDate = document.createElement('td');
         this.tableStartDate.innerHTML = new Date(this.creation_date).toLocaleString();
         
-        // Status
-        let tableStatusCell = document.createElement('td')
-        this.tableStatus = document.createElement('span')
-        tableStatusCell.append(this.tableStatus)
-        
-        this.append(this.tableComment)
-        this.append(this.tableLanguage)
-        this.append(this.tableStartDate)
-        this.append(tableStatusCell)
+        // Duration
+        this.tableDuration = document.createElement('td');
+        this.tableDuration.innerHTML = "1 minute 26 seconds";
 
-        this.setStatus(this.status)
+        // Status
+        let tableStatusCell = document.createElement('td');
+        this.tableStatus = document.createElement('span');
+        tableStatusCell.append(this.tableStatus);
+        
+        this.append(this.tableComment);
+        this.append(this.tableLanguage);
+        this.append(this.tableStudents);
+        this.append(this.tableStartDate);
+        this.append(this.tableDuration);
+        this.append(tableStatusCell);
+
+        this.setStatus(this.status);
     }
 
     setStatus(newStatus){
-        this.tableStatus.innerHTML = statuses[newStatus]
+        this.tableStatus.innerHTML = statuses[newStatus];
         if(newStatus == completedStatus){
-            this.tableComment.innerHTML = `<a href="/jobs/${this.job_id}/result/" style="text-decoration: none;">${this.comment}</a>`
+            this.tableComment.innerHTML = `<a href="/jobs/${this.job_id}/result/" style="text-decoration: none;">${this.comment}</a>`;
         }
 
         this.tableStatus.className = '';
@@ -51,7 +62,7 @@ class Job extends HTMLTableRowElement{
         statusMapping[failedStatus] = 'bg-danger';
         let classes = ['badge', statusMapping[newStatus]];
 
-        this.tableStatus.classList.add(...classes)
+        this.tableStatus.classList.add(...classes);
     }
 }
 customElements.define('job-row', Job, { extends: 'tr' });
--- static/js/jobTable.js
+++ static/js/jobTable.js
@@ -1,72 +1,61 @@
 let jobsTable = document.getElementById('job-table');
 let jobsSearchBar = document.getElementById('job-search-bar');
-
 setupTableSearch(jobsTable, jobsSearchBar);
 
+let jobsTableBody = jobsTable.getElementsByTagName('tbody')[0];
+
 function addJob(job) {
-    document.getElementById('no-jobs-message').style.display = 'none'
+	document.getElementById('no-jobs-message').style.display = 'none'
 
-    let jobTableBody = jobsTable.getElementsByTagName('tbody')[0];
+	// let jobInfo = document.createElement("td");
+	// jobInfo.setAttribute("colspan", "6");
+	// jobInfo.style="padding: 0 !important;";
 
-    let jobInfo = document.createElement("td");
-    jobInfo.setAttribute("colspan", "4");
-    jobInfo.style="padding: 0 !important;";
+	// let jobInfoCollapse = document.createElement("div");
+	// jobInfoCollapse.id = `job-info-${job.job_id}`;
+	// jobInfoCollapse.classList.add("collapse");
+	// jobInfo.append(jobInfoCollapse);
 
-    let jobAccordion = document.createElement("div");
-    jobAccordion.id = `job-accordion-${job.job_id}`;
-    jobAccordion.classList.add("collapse");
-    jobInfo.append(jobAccordion);
+	// Info
+	// let jobStatusMap = document.createElement("div");
+	// jobStatusMap.classList.add("d-flex");
+	// jobStatusMap.classList.add("flex-column");
+	// jobInfoCollapse.append(jobStatusMap);
 
-
-    // TEST
-    let label = document.createElement("label");
-    label.style.height = "200px";
-    label.innerText = "Test";
-    jobAccordion.append(label);
-
-    
-    jobTableBody.prepend(jobInfo);
-    jobTableBody.prepend(new Job(job));
+	// jobsTableBody.prepend(jobInfo);
+	jobsTableBody.prepend(new Job(job));
 }
-
 
 const terminalStates = [completedStatus, failedStatus];
 function isTerminalState(state){
-    return terminalStates.includes(state)
+	return terminalStates.includes(state)
 }
 
 let unfinishedJobs = [];
 let result = fetch(GET_JOBS_URL).then(async (response)=>{
-    let json = await response.json()
-
-    json.forEach(item => {
-        addJob(item);
-        if (!isTerminalState(item.status)){
-            unfinishedJobs.push(item.job_id)
-        }
-    });
-    if(json.length == 0){
-        document.getElementById('no-jobs-message').style.display = 'block'
-    }
+	let json = await response.json()
+	json.forEach(item => {
+		addJob(item);
+		if (!isTerminalState(item.status)){
+			unfinishedJobs.push(item.job_id)
+		}
+	});
+	if(json.length == 0){
+		document.getElementById('no-jobs-message').style.display = 'block';
+	}
 });
 
-setInterval(async function() {
-    if(unfinishedJobs.length == 0) {
-        return;
-    }
-    let result = await fetch(
-        GET_JOB_STATUSES_URL + "?" + new URLSearchParams({job_ids: unfinishedJobs})
-    )
-
-    let json = await result.json();
-
-    for (let key in json) {
-        var value = json[key];
-
-        document.querySelector(`tr[job_id="${key}"]`).setStatus(value)
-        if(isTerminalState(value)){
-            unfinishedJobs = unfinishedJobs.filter(item => item !== key)
-        }
-    }
-    
+setInterval(async function(){
+	if(unfinishedJobs.length == 0) {
+		return;
+	}
+	let result = await fetch(GET_JOB_STATUSES_URL + "?" + new URLSearchParams({job_ids: unfinishedJobs}));
+	let json = await result.json();
+	for (let key in json) {
+		var value = json[key];
+		document.querySelector(`tr[job_id="${key}"]`).setStatus(value);
+		if(isTerminalState(value)){
+			unfinishedJobs = unfinishedJobs.filter(item => item !== key);
+		}
+	}
 }, POLLING_TIME);
