0f853b0b40d0eef9a03491370e43e5094da67b14
Joshua Lochner <joshua@joshualochner.com>
2021-10-06 00:00:27 +0200

    Format job submission js file

--- static/js/jobSubmission.js
+++ static/js/jobSubmission.js
@@ -1,9 +1,9 @@
 /**
  * Determines whether or not a file name ends with an extension from a list of given extensions.
  */
-function hasExtension(fileName, extensions){
-	for (var extension of extensions){
-		if (fileName.endsWith("."+extension)){
+function hasExtension(fileName, extensions) {
+	for (var extension of extensions) {
+		if (fileName.endsWith("." + extension)) {
 			return true;
 		}
 	}
@@ -13,7 +13,7 @@ function hasExtension(fileName, extensions){
 /**
  * Determines whether or not a file is an archive.
  */
-function isArchive(fileName){
+function isArchive(fileName) {
 	return hasExtension(fileName, SUPPORTED_ARCHIVES);
 }
 
@@ -21,12 +21,12 @@ function isArchive(fileName){
  * Determines whether or not a file is a source file. If a programming language code is specified,
  * this will return if it is a source file for that particular language.
  */
-function isSource(fileName, language){
+function isSource(fileName, language) {
 	let extensions = [];
-	if (language != null){
+	if (language != null) {
 		extensions = SUPPORTED_LANGUAGES[language];
-	}else{
-		for (let key of Object.keys(SUPPORTED_LANGUAGES)){
+	} else {
+		for (let key of Object.keys(SUPPORTED_LANGUAGES)) {
 			extensions = extensions.concat(SUPPORTED_LANGUAGES[key][3]);
 		}
 	}
@@ -35,19 +35,19 @@ function isSource(fileName, language){
 
 /**
  * Returns a file's name when given its path.
- */ 
-function getFileNameFromPath(filePath){
-    var dirSepIndex = filePath.lastIndexOf("/");
-    if (dirSepIndex != -1){
-        return filePath.substring(dirSepIndex+1);
-    }
-    return filePath;
+ */
+function getFileNameFromPath(filePath) {
+	var dirSepIndex = filePath.lastIndexOf("/");
+	if (dirSepIndex != -1) {
+		return filePath.substring(dirSepIndex + 1);
+	}
+	return filePath;
 }
 
 /**
  * Renames a given file and returns it.
  */
-function renameFile(file, newName){
+function renameFile(file, newName) {
 	return new File([file], newName, {
 		type: file.type,
 		lastModified: file.lastModified,
@@ -59,13 +59,13 @@ function renameFile(file, newName){
  * Single submissions will contain more source files than archives, while
  * batches will contain more archives than source files.
  */
-async function isSingleSubmission(files){
+async function isSingleSubmission(files) {
 	let numSourceFiles = 0;
 	let numArchives = 0;
-	for (var file of files){
-		if (isSource(file.name)){
+	for (var file of files) {
+		if (isSource(file.name)) {
 			numSourceFiles++;
-		}else if (isArchive(file.name)){
+		} else if (isArchive(file.name)) {
 			numArchives++;
 		}
 	}
@@ -75,7 +75,7 @@ async function isSingleSubmission(files){
 /**
  * Trims the end of a string by n characters.
  */
- function trimRight(str, n){
+function trimRight(str, n) {
 	return str.slice(0, str.length - n)
 }
 
@@ -83,9 +83,9 @@ async function isSingleSubmission(files){
  * Find the first difference between two strings, and return the index of that character.
  * Credit: https://stackoverflow.com/a/32858679/7840247
  */
-function findFirstDiffIndex(a, b){
+function findFirstDiffIndex(a, b) {
 	var longerLength = Math.max(a.length, b.length);
-	for (var i = 0; i < longerLength; i++){
+	for (var i = 0; i < longerLength; i++) {
 		if (a[i] !== b[i]) return i;
 	}
 	return -1;
@@ -99,20 +99,20 @@ function findFirstDiffIndex(a, b){
  * a common directory. The algorithm works as follows:
  * Find two different student archives and then compare their paths to determine the root.
  */
-function getRootIndex(files){
+function getRootIndex(files) {
 	var prevPath = "";
-	for (var file of files){
+	for (var file of files) {
 		var pathWithName = file.name;
 		var name = getFileNameFromPath(pathWithName);
 		var path = trimRight(pathWithName, name.length);
-		if (prevPath != ""){
-			if (path != prevPath){
+		if (prevPath != "") {
+			if (path != prevPath) {
 				var diff = findFirstDiffIndex(path, prevPath);
 				var same = path.substring(0, diff);
-				return same.lastIndexOf("/")+1; // shift left in path to beginning of contained folder
+				return same.lastIndexOf("/") + 1; // shift left in path to beginning of contained folder
 			}
 		}
-		if (isArchive(name)){
+		if (isArchive(name)) {
 			prevPath = path;
 		}
 	}
@@ -122,9 +122,9 @@ function getRootIndex(files){
 /**
  * Reads the data from a file and returns it.
  */
-async function readFileData(file){
+async function readFileData(file) {
 	return new Promise((resolve, reject) => {
-		file.readData(function(data){
+		file.readData(function (data) {
 			resolve(data);
 		});
 	});
@@ -133,10 +133,10 @@ async function readFileData(file){
 /**
  * Extracts all the source files from a single student's archive, and "stitches" them together.
  */
-async function extractSingle(files, language){
+async function extractSingle(files, language) {
 	var buffer = "";
-	for (var file of files){
-		if (isSource(file.name, language)){
+	for (var file of files) {
+		if (isSource(file.name, language)) {
 			buffer += ">>> " + file.name + " <<<\n";
 			buffer += (await new Response(new File([await readFileData(file)], file.name)).text() + "\n\n");
 		}
@@ -151,9 +151,9 @@ async function extractFiles(archive) {
 
 	// Wait until formats have been loaded
 	await loadFormatsPromise;
-	
+
 	// Archive must be a file
-	if (!(archive instanceof File)){
+	if (!(archive instanceof File)) {
 		archive = new File([await readFileData(archive)], archive.name); // Convert "uncompress file" to a file
 	}
 
@@ -167,8 +167,8 @@ async function extractFiles(archive) {
 
 	// Add all file entries in the archive to a list of files
 	let files = [];
-	opened.entries.forEach(function(file) {
-		if(file.is_file){
+	opened.entries.forEach(function (file) {
+		if (file.is_file) {
 			files.push(file);
 		}
 	});
@@ -179,9 +179,9 @@ async function extractFiles(archive) {
  * Extracts multiple archives from a single student.
  * This is necessary, as students may include multiple submission attachments that contain code.
  */
-async function extractMultiple(archives, language){
+async function extractMultiple(archives, language) {
 	var buffer = "";
-	for (var archive of archives){
+	for (var archive of archives) {
 		buffer += await extractSingle(await extractFiles(archive), language);
 	}
 	return buffer;
@@ -204,20 +204,20 @@ async function extractMultiple(archives, language){
  * - Finally, check if the last student had files that weren't extracted (since extractions
  *   only take place on the event of a student change).
  */
-async function extractBatch(files, language, onExtract){
+async function extractBatch(files, language, onExtract) {
 	var prevStudent = "";
 	var rootIndex = getRootIndex(files);
 	var studentArchives = [];
-	for (var file of files){
-		if (isArchive(file.name)){
+	for (var file of files) {
+		if (isArchive(file.name)) {
 			var pathFromStudent = file.name.substring(rootIndex);
 			var pathSepIndex = pathFromStudent.indexOf("/");
 			var student = pathFromStudent; // Batch could just be a folder containing archives.
-			if (pathSepIndex != -1){
+			if (pathSepIndex != -1) {
 				student = pathFromStudent.substring(0, pathSepIndex); // Student's folder name.
 			}
-			if (student != prevStudent){
-				if (prevStudent != ""){
+			if (student != prevStudent) {
+				if (prevStudent != "") {
 					onExtract(prevStudent, await extractMultiple(studentArchives, language));
 					studentArchives = []; // Clear current student's archives.
 				}
@@ -226,7 +226,7 @@ async function extractBatch(files, language, onExtract){
 			studentArchives.push(file); // Record this file as a student's archive.
 		}
 	}
-	if (studentArchives.length > 0){
+	if (studentArchives.length > 0) {
 		onExtract(student, await extractMultiple(studentArchives, language));
 	}
 }
@@ -262,26 +262,26 @@ let jobErrorMessage = document.getElementById("job-error-message");
 let createJobButton = document.getElementById("create-job-button");
 
 let isDisplayingError = false;
-function displayError(message){
-	if (isDisplayingError){
+function displayError(message) {
+	if (isDisplayingError) {
 		return;
 	}
 	jobErrorMessage.textContent = message;
 	createJobModalElement.classList.add("animate__animated", "animate__shakeX");
 	isDisplayingError = true;
 
-	setTimeout(function(){
+	setTimeout(function () {
 		jobErrorMessage.textContent = "";
 		createJobModalElement.classList.remove("animate__animated", "animate__shakeX");
 		isDisplayingError = false;
 	}, 3000);
 }
 
-function updateDropZoneTarget(){
+function updateDropZoneTarget() {
 	jobDropZone.zoneText.innerHTML = `Drag and drop <b>${jobAttachBaseFiles.checked ? "base" : "student"}</b> files here!`;
 }
 
-function setEnabled(isEnabled){
+function setEnabled(isEnabled) {
 	jobDropZone.setInteractable(isEnabled);
 	createJobButton.disabled = !isEnabled;
 }
@@ -291,28 +291,28 @@ createJobForm.onsubmit = async (e) => {
 	e.preventDefault();
 	setEnabled(false);
 
-	try{
+	try {
 		// Create a new form (and capture name, language, max matches until ignored and max matches displayed)
 		let jobFormData = new FormData(createJobForm);
-		
-		function appendFilesToForm(name, data, isBaseFile){
+
+		function appendFilesToForm(name, data, isBaseFile) {
 			jobFormData.append(isBaseFile ? BASE_FILES_NAME : FILES_NAME, new Blob([data]), name);
 		}
 
 		let numStudents = 0;
-		for (let jobDropZoneFile of jobDropZone.files){
+		for (let jobDropZoneFile of jobDropZone.files) {
 			let archive = jobDropZoneFile.file;
 			let languageId = jobLanguage.options[jobLanguage.selectedIndex].getAttribute("language-id");
 			let isBaseFile = jobDropZoneFile.isBaseFile;
 
 			let files = await extractFiles(archive, languageId);
 
-			if (await isSingleSubmission(files, languageId) || isBaseFile){
+			if (await isSingleSubmission(files, languageId) || isBaseFile) {
 				appendFilesToForm(archive.name, await extractSingle(files, languageId), isBaseFile);
-				if (!isBaseFile){
+				if (!isBaseFile) {
 					numStudents++;
 				}
-			}else{
+			} else {
 				let counter = 0;
 				await extractBatch(files, languageId, (name, data) => {
 					appendFilesToForm(name, data, false);
@@ -331,24 +331,27 @@ createJobForm.onsubmit = async (e) => {
 		// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/upload
 		// Other events: error, abort, timeout
 
-		xhr.upload.addEventListener('loadstart', e=>{
+		xhr.upload.addEventListener('loadstart', e => {
 			// The upload has begun.
 		});
-		xhr.upload.addEventListener('progress', e=>{
+		xhr.upload.addEventListener('progress', e => {
 			let percentage = e.lengthComputable ? (e.loaded / e.total) * 100 : 0;
 			// TODO do something with progress information
 		});
-		xhr.upload.addEventListener('load', e=>{
+
+		xhr.upload.addEventListener('load', e => {
 			// The upload completed successfully.
 			// Done uploading, now server is processing upload (writing files to disk)
 		});
 
-		xhr.onreadystatechange = function() { // Call a function when the state changes.
-			if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-				// Obtain job as json data and add to the jobs table
-				let json = xhr.response;
-				addJob(json, true);
-				unfinishedJobs.push(json["job_id"]);
+		xhr.onreadystatechange = function () { // Call a function when the state changes.
+			if (this.readyState === XMLHttpRequest.DONE) {
+				if (this.status === 200) {
+					// Obtain job as json data and add to the jobs table
+					let json = xhr.response;
+					addJob(json, true);
+					unfinishedJobs.push(json["job_id"]);
+				}
 
 				// Hide and reset the form and dropzone
 				createJobModal.hide();
@@ -356,37 +359,37 @@ createJobForm.onsubmit = async (e) => {
 					createJobForm.reset();
 					jobDropZone.reset();
 					updateDropZoneTarget();
+					setEnabled(true);
 				}, 200);
 			}
+
 		}
 		xhr.send(jobFormData);
 
-	}catch (err){
+	} catch (err) {
 		console.err(err);
-	}
-	finally{
 		setEnabled(true);
 	}
 };
 
 jobDropZone.onFileAdded = async (jobDropZoneFile) => {
-	
+
 	let archive = jobDropZoneFile.file;
 	let files = await extractFiles(archive);
 
-	function getExtension(name){
+	function getExtension(name) {
 		return name.split(".").pop();
 	}
 
-	function getProgrammingLanguageId(files){
+	function getProgrammingLanguageId(files) {
 		let d = {};
-		for (let key in SUPPORTED_LANGUAGES){
+		for (let key in SUPPORTED_LANGUAGES) {
 			d[key] = 0;
 		}
-		for (let file of files){
+		for (let file of files) {
 			let extension = getExtension(file.name);
-			for (let key in SUPPORTED_LANGUAGES){
-				if(SUPPORTED_LANGUAGES[key][2].includes(extension)){
+			for (let key in SUPPORTED_LANGUAGES) {
+				if (SUPPORTED_LANGUAGES[key][2].includes(extension)) {
 					d[key] += 1;
 				}
 			}
@@ -399,13 +402,13 @@ jobDropZone.onFileAdded = async (jobDropZoneFile) => {
 
 	// Programming Language
 	let langTestFiles = [...files];
-	if (!isSingle){
+	if (!isSingle) {
 		let counter = 0;
 		let maxArchives = 3;
-		for (let file of files){
-			if (isArchive(file.name)){
+		for (let file of files) {
+			if (isArchive(file.name)) {
 				let tmpArchive = await extractFiles(file);
-				for (let tmpFile of tmpArchive){
+				for (let tmpFile of tmpArchive) {
 					langTestFiles.push(tmpFile);
 				}
 				counter++;
@@ -417,15 +420,15 @@ jobDropZone.onFileAdded = async (jobDropZoneFile) => {
 	let language = SUPPORTED_LANGUAGES[languageId][0];
 
 	// Tags
-	if (jobAttachBaseFiles.checked){
+	if (jobAttachBaseFiles.checked) {
 		jobDropZoneFile.addTag("Base", "var(--bs-dark)");
 		jobDropZoneFile.isBaseFile = true;
-	}else{
+	} else {
 		jobDropZoneFile.addTag(isSingle ? "Single" : `Batch (${files.length})`, "var(--bs-dark)");
 	}
 
-	if (jobDropZone.files.length >= 1){
-		jobName.value = jobName.value || trimRight(archive.name, getExtension(archive.name).length+1);
+	if (jobDropZone.files.length >= 1) {
+		jobName.value = jobName.value || trimRight(archive.name, getExtension(archive.name).length + 1);
 		jobLanguage.value = language;
 		createJobButton.disabled = false;
 	}
