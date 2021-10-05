06fce64acc7cf9fc76cf5312d3b08359c7904db1
Joshua Lochner <joshua@joshualochner.com>
2021-10-06 00:02:38 +0200

    Change to debug mode

--- automoss/settings.py
+++ automoss/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
