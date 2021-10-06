41757fa0b855416043eb9010f1a31dc257718af3
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-10-06 18:37:03 +0200

    Add comments to jobTable.js (and fix bug with info delay on page load)

--- static/js/jobTable.js
+++ static/js/jobTable.js
@@ -1,3 +1,4 @@
+// Setup the jobs table for searching.
 let jobsTable = document.getElementById('job-table');
 let jobsSearchBar = document.getElementById('job-search-bar');
 setupTableSearch(jobsTable, jobsSearchBar);
@@ -6,70 +7,102 @@ let jobsTableBody = jobsTable.getElementsByTagName('tbody')[0];
 let noJobsMessage = document.getElementById('no-jobs-message');
 
 const terminalStates = [completedStatus, failedStatus];
-function isTerminalState(state){
-	return terminalStates.includes(state);
-}
-
-
-let eventStatusMapping = { 
+const timelineEventMapping = {
 	"INQ": 1,
 	"UPL": 2,
 	"PRO": 3,
 	"PAR": 4,
-	"COM": 6
+	"COM": 6 // Don't set to 5 as this implies the "completed" state can be "in progress".
 };
 
+/**
+ * Determine whether a status provided is a terminal status or not.
+ * The terminal states are listed above (i.e., COMPLETED and FAILED).
+ */ 
+function isTerminalState(state){
+	return terminalStates.includes(state);
+}
+
+/**
+ * Return the last event that completed before the job failed.
+ */
 function getLastCompletedEvent(jobId){
 	let prevEvents = document.getElementById(`job-logs-${jobId}`).prevEvents;
 	if (prevEvents != undefined){
-		for (let i = prevEvents.length - 1; i >= 0; i--){
-			if (Object.hasOwn(eventStatusMapping, prevEvents[i])){
-				return eventStatusMapping[prevEvents[i]];
+		for (let i = prevEvents.length - 1; i >= 0; i--){ // Look for the first "valid" completed event.
+			if (Object.hasOwn(timelineEventMapping, prevEvents[i])){
+				return timelineEventMapping[prevEvents[i]];
 			}
 		}
 	}
-	return 0;
+	return 0; // If no previous events, then the job failed to be created.
 }
 
+/**
+ * Update the status (badge and timeline) for a job in the table.
+ */
 function updateJobStatus(jobId, status){
+	// Badge
 	document.querySelector(`tr[job_id="${jobId}"]`).setStatus(status);
 	if(isTerminalState(status)){
 		unfinishedJobs = unfinishedJobs.filter(item => item !== jobId);
 	}
-
+	// Timeline
 	let jobTimeline = document.getElementById(`job-timeline-${jobId}`);
 	if (status != "FAI"){
-		jobTimeline.setCompleted(eventStatusMapping[status]);
+		jobTimeline.setCompleted(timelineEventMapping[status]);
 	}else{
 		jobTimeline.setFailed(getLastCompletedEvent(jobId));
 	}
 }
 
+/**
+ * Update the logs for a job in the table.
+ */
 function updateJobLogs(jobId, logs){
 	let jobLogs = document.getElementById(`job-logs-${jobId}`);
 
 	let tmpLogs = "";
 	jobLogs.prevEvents = [];
 
+	// Format the logs, which were retrieved as a json object.
 	for (let log in logs){
 		tmpLogs += logs[log].str + "\n";
-
 		if (logs[log].type){
-			jobLogs.prevEvents.push(logs[log].type);
+			jobLogs.prevEvents.push(logs[log].type); // Record the previous events (for status update above).
 		}
 	}
-	tmpLogs = trimRight(tmpLogs, 1);
+	tmpLogs = trimRight(tmpLogs, 1); // Remove the last newline character.
 
+	// Set the logs only if they changed.
 	if (tmpLogs !== jobLogs.prevLogs){
 		jobLogs.prevLogs = jobLogs.innerHTML = tmpLogs;
 	}
 }
 
-function updateJobs(jobs){
-	updateJobsTable(GET_JOB_STATUSES_URL, jobs, updateJobStatus);
-	updateJobsTable(GET_JOB_LOGS_URL, jobs, updateJobLogs);
+/**
+ * Retrieve a list of jobs (based on the list of ids provided) and perform an operation on each of them.
+ */
+async function performOperationOnJobs(url, jobIds, operation){
+	let result = await fetch(url + "?" + new URLSearchParams({job_ids: jobIds}));
+	let json = await result.json();
+	for (let key in json){
+		operation(key, json[key]);
+	}
 }
 
+/**
+ * Update all the jobs in the table.
+ */
+function updateJobs(jobs){
+	performOperationOnJobs(GET_JOB_STATUSES_URL, jobs, updateJobStatus);
+	performOperationOnJobs(GET_JOB_LOGS_URL, jobs, updateJobLogs);
+}
+
+/**
+ * Add a job to the jobs table. If force open is set, the job's info collapsible will be toggled
+ * open by default. (Necessary when creating the job using the job submission modal).
+ */
 function addJob(job, forceOpen=false){
 	noJobsMessage.style.display = 'none';
 
@@ -131,29 +164,22 @@ let result = fetch(GET_JOBS_URL).then(async (response)=>{
 		addJob(item);
 		if (!isTerminalState(item.status)){
 			unfinishedJobs.push(item.job_id);
-		}else{
-			updateJobs([item.job_id]);
 		}
+		updateJobs([item.job_id]); // Immediately update all job logs and statuses on load.
 	});
 	if(json.length == 0){
 		noJobsMessage.style.display = 'block';
 	}
 });
 
-async function updateJobsTable(url, jobs, f){
-	let result = await fetch(url + "?" + new URLSearchParams({job_ids: jobs}));
-	let json = await result.json();
-	for (let key in json){
-		f(key, json[key]);
-	}
-}
-
+// Update the status and logs of unfinished jobs in the table.
 setInterval(async function(){
 	if(unfinishedJobs.length != 0){		
 		updateJobs(unfinishedJobs);
 	}
 }, POLLING_TIME);
 
+// Update the duration of unfinished jobs in the table every second.
 setInterval(async function(){
 	for (let jobId of unfinishedJobs){
 		let job = document.getElementById(`job-${jobId}`);
