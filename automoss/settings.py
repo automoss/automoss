a7db7e01c12ac990c72cd5d367fe194c193d62f4
Carl Combrinck <carljcombrinck@gmail.com>
2021-10-06 19:33:05 +0200

    Add HOSTNAME environment variable

--- automoss/settings.py
+++ automoss/settings.py
@@ -29,6 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+# Hostname
+HOSTNAME = os.getenv("HOST_NAME")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
