27b2cb5e8bf34238235045a9b1580e5266eaec5c
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-10-06 17:28:18 +0200

    Add comments to dropzone file

--- static/js/dropZoneFile.js
+++ static/js/dropZoneFile.js
@@ -1,7 +1,7 @@
 class DropZoneFile extends HTMLElement {
-    constructor(file, dropZone) {
-        super();
-        this.file = file;
+	constructor(file, dropZone) {
+		super();
+		this.file = file;
 
 		this.classList.add("position-relative");
 		this.classList.add("d-flex");
@@ -10,15 +10,15 @@ class DropZoneFile extends HTMLElement {
 
 		// Progress Bar
 		this.progressBar = document.createElement("div");
-        this.progressBar.id = "progress-bar";
-        this.progressBar.classList.add("position-absolute");
+		this.progressBar.id = "progress-bar";
+		this.progressBar.classList.add("position-absolute");
 		this.progressBar.classList.add("progress-bar");
 		this.progressBar.classList.add("bg-secondary");
 		this.append(this.progressBar);
 
 		// Info
 		this.info = document.createElement("div");
-        this.info.id = "info";
+		this.info.id = "info";
 		this.info.classList.add("d-flex");
 		this.info.classList.add("w-100");
 		this.info.classList.add("p-2");
@@ -26,7 +26,7 @@ class DropZoneFile extends HTMLElement {
 
 		// Info > Name
 		this.name = document.createElement("label");
-        this.name.id = "name";
+		this.name.id = "name";
 		this.name.textContent = file.name + ` (${dropZone.getFileSize(file.size)})`;
 		this.name.classList.add("flex-fill");
 		this.info.append(this.name);
@@ -38,26 +38,35 @@ class DropZoneFile extends HTMLElement {
 
 		// Info > Remove Button
 		this.removeButton = document.createElement("button");
-        this.removeButton.id = "remove-button";
-        this.removeButton.type = "button";
+		this.removeButton.id = "remove-button";
+		this.removeButton.type = "button";
 		this.removeButton.addEventListener("click", () => dropZone.removeFile(this));
 		this.info.append(this.removeButton);
 
-        // Info > Remove Button > Icon
-        this.removeButtonIcon = document.createElement("img");
+		// Info > Remove Button > Icon
+		this.removeButtonIcon = document.createElement("img");
 		this.removeButtonIcon.src = "/static/img/x.svg";
-        this.removeButton.append(this.removeButtonIcon);
-    }
+		this.removeButton.append(this.removeButtonIcon);
+	}
 
+	/**
+	 * Sets the removable state.
+	 */
 	setRemovable(isRemovable){
 		this.removeButton.disabled = !isRemovable;
 		this.removeButtonIcon.style.opacity = isRemovable ? 1 : 0.25;
 	}
 
-    setProgress(progress) {
-        this.progressBar.style.width = `${progress*100}%`;
-    }
+	/**
+	 * Sets the progress bar value.
+	 */
+	setProgress(progress) {
+		this.progressBar.style.width = `${progress*100}%`;
+	}
 
+	/**
+	 * Adds a tag.
+	 */
 	addTag(name, colour){
 		let tag = document.createElement("span");
 		tag.classList.add("badge");
