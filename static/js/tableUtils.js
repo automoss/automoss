130ebcd8f245360946e81af0cebcdd038386e0a8
Daniel Lochner <daniellochner@users.noreply.github.com>
2021-10-06 19:07:56 +0200

    Convert spaces indentation to tabs

--- static/js/tableUtils.js
+++ static/js/tableUtils.js
@@ -1,33 +1,33 @@
-function getText(node) {
-    let text = '';
-    if (node.nodeType === document.TEXT_NODE){
-        text += node.nodeValue + ' ';
-    }else{
-        for (let child of node.childNodes){
-            text += this.getText(child);
-        }
-    }
-    return text;
+function getText(node){
+	let text = '';
+	if (node.nodeType === document.TEXT_NODE){
+		text += node.nodeValue + ' ';
+	}else{
+		for (let child of node.childNodes){
+			text += this.getText(child);
+		}
+	}
+	return text;
 }
 function contains(row, text){
-    return getText(row).toLowerCase().includes(text.toLowerCase());
+	return getText(row).toLowerCase().includes(text.toLowerCase());
 }
 function show(row){
-    row.style.display = "table-row";
+	row.style.display = "table-row";
 }
 function hide(row){
-    row.style.display = "none";
+	row.style.display = "none";
 }
 
 function setupTableSearch(table, searchBar){
-    searchBar.oninput = function(){
-        for (let row of table.tBodies[0].children){
-            if(contains(row, searchBar.value)){
-                show(row);
-            }else{
-                hide(row);
-            }
-        }
-    }
+	searchBar.oninput = function(){
+		for (let row of table.tBodies[0].children){
+			if(contains(row, searchBar.value)){
+				show(row);
+			}else{
+				hide(row);
+			}
+		}
+	}
 }
 window.setupTableSearch = setupTableSearch;
